bb0c19ebffc670af7cd0c21188c1f71344bbd7c4
Danny  Crisostomo <146300252+DannyCrisostomo@users.noreply.github.com>
2023-12-31 08:13:26 -0500

    Add files via upload

---
+++ Login/login/Read/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Read
+{
+    class Conexion
+    {
+        public static SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection("Server=DESKTOP-3RH2L63\\SQLEXPRESS;Database= Registro; integrated security=true");
+            cn.Open();
+            return cn;
+        }
+    }
+}
---
+++ Login/login/Read/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Read
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Conexion.Conectar();
+            MessageBox.Show("bd conectado");
+
+            dataGridView1.DataSource = llenar_grid(); 
+        }
+
+        public DataTable llenar_grid()
+        {
+            Conexion.Conectar();
+            DataTable dt = new DataTable();
+            string consulta = "SELECT * FROM ALUMNO";
+            SqlCommand cmd = new SqlCommand(consulta, Conexion.Conectar());
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
